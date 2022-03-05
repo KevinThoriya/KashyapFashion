061f4c3ba7f6c661740f57db227a0676a62ee6fb
Kevin Thoriya <kevin.thoriya@biznovare.com>
2022-03-05 12:36:47 +0530

    - commenting all routes

--- backend2/src/routes.js
+++ backend2/src/routes.js
@@ -48,193 +48,193 @@ const cache = apicache.middleware("1 minute", null, {
 });
 
 // BUSCA, ADICIONA, ALTERA OU REMOVE USUÁRIOS
-router.get(
-  "/users",
-  userValidator.list,
-  adminJwtAuthentication,
-  userController.list
-);
-router.get(
-  "/users/:id",
-  userValidator.show,
-  jwtAuthentication,
-  userController.show
-);
-router.post("/users", userValidator.store, userController.store);
-router.put(
-  "/users",
-  userValidator.update,
-  jwtAuthentication,
-  userController.update
-);
-router.delete(
-  "/users",
-  userValidator.destroy,
-  jwtAuthentication,
-  userController.destroy
-);
+// router.get(
+//   "/users",
+//   userValidator.list,
+//   adminJwtAuthentication,
+//   userController.list
+// );
+// router.get(
+//   "/users/:id",
+//   userValidator.show,
+//   jwtAuthentication,
+//   userController.show
+// );
+// router.post("/users", userValidator.store, userController.store);
+// router.put(
+//   "/users",
+//   userValidator.update,
+//   jwtAuthentication,
+//   userController.update
+// );
+// router.delete(
+//   "/users",
+//   userValidator.destroy,
+//   jwtAuthentication,
+//   userController.destroy
+// );
 
-// BUSCA, ADICIONA, ALTERA OU REMOVE ENDEREÇOS DE UM USUÁRIO
-router.get(
-  "/addresses",
-  addressValidator.list,
-  jwtAuthentication,
-  addressController.list
-);
-router.post(
-  "/addresses",
-  addressValidator.store,
-  jwtAuthentication,
-  addressController.store
-);
-router.put(
-  "/addresses/:id",
-  addressValidator.update,
-  jwtAuthentication,
-  addressController.update
-);
-router.delete(
-  "/addresses/:id",
-  addressValidator.destroy,
-  jwtAuthentication,
-  addressController.destroy
-);
+// // BUSCA, ADICIONA, ALTERA OU REMOVE ENDEREÇOS DE UM USUÁRIO
+// router.get(
+//   "/addresses",
+//   addressValidator.list,
+//   jwtAuthentication,
+//   addressController.list
+// );
+// router.post(
+//   "/addresses",
+//   addressValidator.store,
+//   jwtAuthentication,
+//   addressController.store
+// );
+// router.put(
+//   "/addresses/:id",
+//   addressValidator.update,
+//   jwtAuthentication,
+//   addressController.update
+// );
+// router.delete(
+//   "/addresses/:id",
+//   addressValidator.destroy,
+//   jwtAuthentication,
+//   addressController.destroy
+// );
 
-// BUSCA PARCELAS DO CARTÃO DE CREDITO
-router.post(
-  "/installments",
-  installmentsValidator.show,
-  installmentsController.show
-);
+// // BUSCA PARCELAS DO CARTÃO DE CREDITO
+// router.post(
+//   "/installments",
+//   installmentsValidator.show,
+//   installmentsController.show
+// );
 
-// BUSCA E ADICIONA ORDERS DE COMPRA DE UM USUÁRIO
-router.get(
-  "/orders",
-  orderValidator.list,
-  jwtAuthentication,
-  orderController.list
-);
-router.get(
-  "/orders/:id",
-  orderValidator.show,
-  jwtAuthentication,
-  orderController.show
-);
-router.post(
-  "/orders",
-  orderValidator.store,
-  jwtAuthentication,
-  orderController.store
-);
+// // BUSCA E ADICIONA ORDERS DE COMPRA DE UM USUÁRIO
+// router.get(
+//   "/orders",
+//   orderValidator.list,
+//   jwtAuthentication,
+//   orderController.list
+// );
+// router.get(
+//   "/orders/:id",
+//   orderValidator.show,
+//   jwtAuthentication,
+//   orderController.show
+// );
+// router.post(
+//   "/orders",
+//   orderValidator.store,
+//   jwtAuthentication,
+//   orderController.store
+// );
 
-// PAGA UMA ORDER DE COMPRA
-router.post(
-  "/orders/:id/payment",
-  orderPaymentValidator.store,
-  jwtAuthentication,
-  orderPaymentController.store
-);
+// // PAGA UMA ORDER DE COMPRA
+// router.post(
+//   "/orders/:id/payment",
+//   orderPaymentValidator.store,
+//   jwtAuthentication,
+//   orderPaymentController.store
+// );
 
-// BUSCA, ALTERA OU REMOVE PEDIDOS DE UM USUÁRIO POR UM ADMIN
-router.get(
-  "/admin/orders",
-  orderAdminValidator.list,
-  adminJwtAuthentication,
-  orderAdminController.list
-);
-router.put(
-  "/admin/orders/:id",
-  orderAdminValidator.update,
-  adminJwtAuthentication,
-  orderAdminController.update
-);
-router.delete(
-  "/admin/orders/:id",
-  orderAdminValidator.destroy,
-  adminJwtAuthentication,
-  orderAdminController.destroy
-);
+// // BUSCA, ALTERA OU REMOVE PEDIDOS DE UM USUÁRIO POR UM ADMIN
+// router.get(
+//   "/admin/orders",
+//   orderAdminValidator.list,
+//   adminJwtAuthentication,
+//   orderAdminController.list
+// );
+// router.put(
+//   "/admin/orders/:id",
+//   orderAdminValidator.update,
+//   adminJwtAuthentication,
+//   orderAdminController.update
+// );
+// router.delete(
+//   "/admin/orders/:id",
+//   orderAdminValidator.destroy,
+//   adminJwtAuthentication,
+//   orderAdminController.destroy
+// );
 
-// UPDATE DE SENHA POR EMAIL ("PERDEU A SENHA?")
-router.post(
-  "/reset-password",
-  userResetPasswordValidator.store,
-  userResetPasswordController.store
-);
-router.put(
-  "/reset-password",
-  userResetPasswordValidator.update,
-  userResetPasswordController.update
-);
+// // UPDATE DE SENHA POR EMAIL ("PERDEU A SENHA?")
+// router.post(
+//   "/reset-password",
+//   userResetPasswordValidator.store,
+//   userResetPasswordController.store
+// );
+// router.put(
+//   "/reset-password",
+//   userResetPasswordValidator.update,
+//   userResetPasswordController.update
+// );
 
-// RETORNA UMA CHAVE JWT
-router.post("/sessions", sessionValidator.store, sessionController.store);
+// // RETORNA UMA CHAVE JWT
+// router.post("/sessions", sessionValidator.store, sessionController.store);
 
-// BUSCA, ADICIONA, ALTERA OU REMOVE UM PRODUTO
-router.get("/products", productValidator.list, cache, productController.list);
-router.get(
-  "/products/:id",
-  productValidator.show,
-  cache,
-  productController.show
-);
-router.post(
-  "/products",
-  productValidator.store,
-  adminJwtAuthentication,
-  productController.store
-);
-router.put(
-  "/products/:id",
-  productValidator.update,
-  adminJwtAuthentication,
-  productController.update
-);
-router.delete(
-  "/products/:id",
-  productValidator.destroy,
-  adminJwtAuthentication,
-  productController.destroy
-);
+// // BUSCA, ADICIONA, ALTERA OU REMOVE UM PRODUTO
+// router.get("/products", productValidator.list, cache, productController.list);
+// router.get(
+//   "/products/:id",
+//   productValidator.show,
+//   cache,
+//   productController.show
+// );
+// router.post(
+//   "/products",
+//   productValidator.store,
+//   adminJwtAuthentication,
+//   productController.store
+// );
+// router.put(
+//   "/products/:id",
+//   productValidator.update,
+//   adminJwtAuthentication,
+//   productController.update
+// );
+// router.delete(
+//   "/products/:id",
+//   productValidator.destroy,
+//   adminJwtAuthentication,
+//   productController.destroy
+// );
 
-// ADICIONA OU REMOVE UMA IMAGEM DO PRODUTO
-router.post(
-  "/products/:id/images",
-  imageValidator.store,
-  adminJwtAuthentication,
-  multer(multerConfig).any(),
-  multerErrorHandler,
-  imageController.store
-);
-router.delete(
-  "/products/images/:id",
-  imageValidator.destroy,
-  adminJwtAuthentication,
-  imageController.destroy
-);
+// // ADICIONA OU REMOVE UMA IMAGEM DO PRODUTO
+// router.post(
+//   "/products/:id/images",
+//   imageValidator.store,
+//   adminJwtAuthentication,
+//   multer(multerConfig).any(),
+//   multerErrorHandler,
+//   imageController.store
+// );
+// router.delete(
+//   "/products/images/:id",
+//   imageValidator.destroy,
+//   adminJwtAuthentication,
+//   imageController.destroy
+// );
 
-// BUSCA, ADICIONA, ALTERA OU REMOVE UMA CATEGORIA
-router.get("/categories", categoryController.list);
-router.post(
-  "/categories",
-  categoryValidator.store,
-  adminJwtAuthentication,
-  categoryController.store
-);
-router.put(
-  "/categories/:id",
-  categoryValidator.update,
-  adminJwtAuthentication,
-  categoryController.update
-);
-router.delete(
-  "/categories/:id",
-  categoryValidator.destroy,
-  adminJwtAuthentication,
-  categoryController.destroy
-);
+// // BUSCA, ADICIONA, ALTERA OU REMOVE UMA CATEGORIA
+// router.get("/categories", categoryController.list);
+// router.post(
+//   "/categories",
+//   categoryValidator.store,
+//   adminJwtAuthentication,
+//   categoryController.store
+// );
+// router.put(
+//   "/categories/:id",
+//   categoryValidator.update,
+//   adminJwtAuthentication,
+//   categoryController.update
+// );
+// router.delete(
+//   "/categories/:id",
+//   categoryValidator.destroy,
+//   adminJwtAuthentication,
+//   categoryController.destroy
+// );
 
-// CALCULO DE FRETE
-router.post("/freight", freightValidator.store, freightController.store);
+// // CALCULO DE FRETE
+// router.post("/freight", freightValidator.store, freightController.store);
 
 module.exports = router;
