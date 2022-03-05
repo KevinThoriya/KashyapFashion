f65f73d50996c580b6d94f9eddcb530202b0db0c
Kevin Thoriya <kevin.thoriya@biznovare.com>
2022-03-05 18:13:30 +0530

    - change the layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
+import { SnackbarProvider } from 'notistack'
+import ReactThemeProvider from '../src/components/ThemeProvider'
+import { UserContextProvider } from '../src/Hooks/userProvider'
 import '../styles/globals.css'
 
+
+export const Providers = ({ children }) => {
+  return (
+    <UserContextProvider>
+      <ReactThemeProvider>
+        <SnackbarProvider
+          // action={
+          //   <div
+          //     className="p-2 stroke-white cursor-pointer"
+          //   >
+          //     <ClosedCaptionRounded size={20} />
+          //   </div>
+          // }
+          dense
+          maxSnack={5}
+          preventDuplicate
+          autoHideDuration={5000}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+        >
+          {children}
+        </SnackbarProvider>
+      </ReactThemeProvider>
+    </UserContextProvider>
+  )
+}
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Providers>
+      <Component {...pageProps} />
+    </Providers>
+  )
 }
 
 export default MyApp
